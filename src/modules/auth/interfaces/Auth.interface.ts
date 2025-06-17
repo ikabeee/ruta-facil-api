d9d85eefd983e6f403ed2444b7c2495298aee76b
ikabeee <carlglz30@gmail.com>
2025-06-17 06:50:23 -0500

    feat: refactor user model and remove deprecated authentication logic

--- src/modules/auth/interfaces/Auth.interface.ts
+++
@@ -1,7 +0,0 @@
-import { LoginDto, AuthResponse } from '../../../common/interfaces/auth.types';
-
-export interface IAuthService {
-  login(loginDto: LoginDto): Promise<AuthResponse>;
-  validateToken(token: string): Promise<boolean>;
-  generateToken(payload: any): string;
-}
