55b2385b4058ad75bef92c0b74dee4589282e389
ikabeee <carlglz30@gmail.com>
2025-06-30 12:37:09 -0500

    feat: implement user retrieval functionality with error handling and response structure

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,4 +1,19 @@
 import { Request, Response } from "express";
+import { UserServiceInterface } from "./interfaces/UserService.interface";
+import { ApiResponse } from "../../shared/helpers/ApiResponse";
+import { ApiError } from "../../shared/errors/ApiError";
 
 export class UserController {
+    constructor(private readonly userService: UserServiceInterface) {}
+    async findAllUsers(_req: Request, res: Response): Promise<Response>{
+        try {
+            const users = await this.userService.findAllUsers();
+            return ApiResponse.success(res, users, 200);
+        } catch (error: any) {
+            if(error instanceof ApiError){
+                return ApiResponse.error(res, error.message, error.statusCode);
+            }
+            return ApiResponse.error(res, error.message, 500);
+        }
+    }
 }
--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,4 +1,14 @@
 import express from 'express';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
+import { PrismaClient } from '../../../generated/prisma';
+const prisma = new PrismaClient()
+const userRepository = new UserRepository(prisma)
+const userService = new UserService(userRepository)
+const userController = new UserController(userService);
 const router = express.Router();
+
+router.get('/', userController.findAllUsers())
 
 export default router;
