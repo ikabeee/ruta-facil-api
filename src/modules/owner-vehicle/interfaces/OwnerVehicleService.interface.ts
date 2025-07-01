0507c764c3483024dbc10b5041cb06cbf32e1d6d
ikabeee <carlglz30@gmail.com>
2025-06-30 23:13:42 -0500

    refactor: remove unnecessary blank line in OwnerVehicleService interface

--- src/modules/owner-vehicle/interfaces/OwnerVehicleService.interface.ts
+++ src/modules/owner-vehicle/interfaces/OwnerVehicleService.interface.ts
@@ -1,7 +1,6 @@
 import { OwnerVehicle } from "../../../../generated/prisma";
 import { CreateOwnerVehicleDto } from "../dto/create-owner-vehicle.dto";
 import { UpdateOwnerVehicleDto } from "../dto/update-owner-vehicle.dto";
-
 export interface OwnerVehicleServiceInterface {
     findAllOwnerVehicles(): Promise<OwnerVehicle[]>;
     findOwnerVehicleById(id: number): Promise<OwnerVehicle>;
