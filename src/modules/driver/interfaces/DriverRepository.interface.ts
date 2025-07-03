352e9afafa4fc236c5368874622d568e9edbc9a8
kmlb <tsssblmk@gmail.com>
2025-07-03 12:35:33 -0600

    comentarios

--- src/modules/driver/interfaces/DriverRepository.interface.ts
+++ src/modules/driver/interfaces/DriverRepository.interface.ts
@@ -3,10 +3,10 @@ import { CreateDriverDto } from "../dto/create-driver.dto";
 import { UpdateDriverDto } from "../dto/update-driver.dto";
 
 export interface DriverRepositoryInterface {
-    findAll(): Promise<Driver[]>;
-    findById(id: number): Promise<Driver>;
-    findByUserId(userId: number): Promise<Driver | null>;
-    createDriver(driverData: CreateDriverDto): Promise<Driver>;
-    updateDriver(id: number, driverData: UpdateDriverDto): Promise<Driver>;
-    deleteDriver(id: number): Promise<void>;
+    findAll(): Promise<Driver[]>;//buscar todos los conductores
+    findById(id: number): Promise<Driver>;// buscar conductor por id
+    findByUserId(userId: number): Promise<Driver | null>;// buscar conductor por id de usuario, puede ser null si no existe
+    createDriver(driverData: CreateDriverDto): Promise<Driver>;// crear conductor
+    updateDriver(id: number, driverData: UpdateDriverDto): Promise<Driver>;// actualizar conductor
+    deleteDriver(id: number): Promise<void>;// eliminar conductor
 }
--- src/modules/driver/interfaces/DriverService.interface.ts
+++ src/modules/driver/interfaces/DriverService.interface.ts
@@ -3,10 +3,10 @@ import { CreateDriverDto } from "../dto/create-driver.dto";
 import { UpdateDriverDto } from "../dto/update-driver.dto";
 
 export interface DriverServiceInterface {
-    findAllDrivers(): Promise<Driver[]>;
-    findDriverById(id: number): Promise<Driver>;
-    findDriverByUserId(userId: number): Promise<Driver | null>;
-    createDriver(driverData: CreateDriverDto): Promise<Driver>;
-    updateDriver(id: number, driverData: UpdateDriverDto): Promise<Driver>;
-    deleteDriver(id: number): Promise<void>;
+    findAllDrivers(): Promise<Driver[]>;//buscar todos los conductores
+    findDriverById(id: number): Promise<Driver>;// buscar conductor por id
+    findDriverByUserId(userId: number): Promise<Driver | null>; // buscar conductor por id de usuario buscar conductor por id de usuario, puede ser null si no existe
+    createDriver(driverData: CreateDriverDto): Promise<Driver>; // crear conductor
+    updateDriver(id: number, driverData: UpdateDriverDto): Promise<Driver>;// actualizar conductor
+    deleteDriver(id: number): Promise<void>;// eliminar conductor
 }
